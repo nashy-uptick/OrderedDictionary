4c4edd52a3510da57201473c28bacaf4a5382ba9
Robert Nash <robert.nash@uptickhq.com>
2024-07-24 07:12:59 +0100

    Wrapped in xcode project.

---
+++ UPTOrderedDictionary/UPTOrderedDictionary.h
@@ -0,0 +1,18 @@
+//
+//  UPTOrderedDictionary.h
+//  UPTOrderedDictionary
+//
+//  Created by Rob Nash on 24/07/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UPTOrderedDictionary.
+FOUNDATION_EXPORT double UPTOrderedDictionaryVersionNumber;
+
+//! Project version string for UPTOrderedDictionary.
+FOUNDATION_EXPORT const unsigned char UPTOrderedDictionaryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UPTOrderedDictionary/PublicHeader.h>
+
+
